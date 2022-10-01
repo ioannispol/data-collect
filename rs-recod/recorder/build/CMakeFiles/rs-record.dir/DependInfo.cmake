3a76776248ad14d84abd4d9ca79eb1b3496117fb
ioannispol <giannis.polymenis@gmail.com>
2022-10-01 19:35:24 +0100

    update - 01102022rs-record

---
+++ rs-recod/recorder/build/CMakeFiles/rs-record.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ioannis/Documents/01-Projects/PhD/Year 3/D435/datacollect/data-collect/rs-recod/recorder/rs-record.cpp" "CMakeFiles/rs-record.dir/rs-record.cpp.o" "gcc" "CMakeFiles/rs-record.dir/rs-record.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rs-recod/recorder/build/CMakeFiles/rs-record.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rs-record.dir/rs-record.cpp.o"
+  "CMakeFiles/rs-record.dir/rs-record.cpp.o.d"
+  "rs-record"
+  "rs-record.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rs-record.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
